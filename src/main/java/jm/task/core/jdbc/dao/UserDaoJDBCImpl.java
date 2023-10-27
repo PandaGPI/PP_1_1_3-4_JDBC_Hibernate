a4aa93f7ed3255e07b7b2f2943a8ab5e26468aee
PandaGPI <pirograg@gmail.com>
2023-10-27 22:57:48 +0300

    TASK001: 1. Реализован класс Util (соединение с базой данных); 2. Реализован класс Service; 3. В DAO реализован метод создания таблицы User

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,16 +1,29 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserDaoJDBCImpl implements UserDao {
+public class UserDaoJDBCImpl extends Util implements UserDao {
+    Connection connection = getConnection();
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        PreparedStatement preparedStatement = null;
+        String sqlQuery = "CREATE TABLE User (id int, name VARCHAR(255), lastName VARCHAR(255), age int)";
+        try {
+            preparedStatement = connection.prepareStatement(sqlQuery);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,53 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collection;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/idea";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASS = "kp0kyc86ufsin";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASS);
+            Class.forName(DB_DRIVER);
+            System.out.println("Database connected!");
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+        return connection;
+    }
+
+
+
+//    // Connect to MySQL
+//    public static Connection getMySQLConnection() throws SQLException,
+//            ClassNotFoundException {
+//
+//        final String hostName = "localhost";
+//        final String dbName = "idea";
+//        final String userName = "root";
+//        final String password = "kp0kyc86ufsin";
+//
+//        return getMySQLConnection(hostName, dbName, userName, password);
+//    }
+//
+//    public static Connection getMySQLConnection(String hostName, String dbName,
+//                                                String userName, String password) throws SQLException, ClassNotFoundException {
+//
+//        Class.forName("com.mysql.jdbc.Driver");
+//        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+//
+//        try (Connection conn = DriverManager.getConnection(connectionURL, userName, password)) {
+//            System.out.println("Database connected!");
+//            return conn;
+//        } catch (SQLException e) {
+//            throw new IllegalStateException("Cannot connect the database!", e);
+//        }
+//    }
 }
